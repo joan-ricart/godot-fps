99b73d953ca4abe5dfbba61a988b7b8fe0577968
Joan <j.ricartt@gmail.com>
2023-10-25 14:27:17 +0200

    yis

--- scenes/Weapon.gd
+++ scenes/Weapon.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 class_name Weapon
 
-enum FireMode {SINGLE, BURST, AUTO}
+enum FireMode {SINGLE, AUTO}
 
 @export var weapon_name : String
 @export var damage : int
@@ -13,14 +13,47 @@ enum FireMode {SINGLE, BURST, AUTO}
 @export var weight : int
 @export var mesh : MeshInstance3D
 
+@onready var ammo := mag_size
+var fire_rate : float
+var time := 0.0
+
 func _ready():
 	self.hide()
+	if fire_mode == FireMode.AUTO:
+		fire_rate = (1000/(rpm/60.0))*0.001
 
-func shoot():
+func shoot(delta):
 	match fire_mode:
 		FireMode.SINGLE:
-			print('Shoot once')
-		FireMode.BURST:
-			print('Burst')
+			if (ammo == 0):
+				print('Out of ammo')
+				return
+
+			if (ammo > 0):
+				ammo -= 1
+				print('shoot - ammo left: ', ammo)
+
 		FireMode.AUTO:
-			print('Spray and pray')
+			if (ammo == 0):
+				print('Out of ammo')
+				return
+
+			if (ammo > 0):
+				if time == 0 or time >= fire_rate:
+					if time != 0:
+						time -= fire_rate
+					ammo -= 1
+					print('shoot - ammo left: ', ammo)
+					
+				time += delta
+
+func reload():
+	if ammo < mag_size:
+		ammo = mag_size
+		print("Reloaded. Current ammo: ", ammo)
+	else:
+		print("Full ammo")
+
+func reset_time():
+	time = 0.0
+	print("Timer reset")
